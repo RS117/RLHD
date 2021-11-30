03a4f42a95eecd6e586e895a0e36a30fb6e8b792
Kyle <mcgoughboy@gmail.com>
2021-11-30 14:04:56 -0800

    add 16384 shadow resolution (#185)

--- src/main/java/rs117/hd/config/ShadowResolution.java
+++ src/main/java/rs117/hd/config/ShadowResolution.java
@@ -34,7 +34,8 @@ public enum ShadowResolution
 	RES_1024("Low (1024)", 1024),
 	RES_2048("Medium (2048)", 2048),
 	RES_4096("High (4096)", 4096),
-	RES_8192("Ultra (8192)", 8192);
+	RES_8192("Ultra (8192)", 8192),
+	RES_16384("Extreme (16384)", 16384);
 
 	private final String name;
 	private final int value;
