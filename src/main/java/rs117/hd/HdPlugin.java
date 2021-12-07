ff6b7ac8e1ad02a05be4fa7315724dee96eb8573
Hooder <ahooder@protonmail.com>
2021-12-07 22:53:41 +0100

    Bind dummy shadow map texture when shadows are disabled

--- src/main/java/rs117/hd/HdPlugin.java
+++ src/main/java/rs117/hd/HdPlugin.java
@@ -1098,6 +1098,7 @@ public class HdPlugin extends Plugin implements DrawCallbacks
 	{
 		if (!configShadowsEnabled)
 		{
+			initDummyShadowMap();
 			return;
 		}
 
@@ -1125,6 +1126,21 @@ public class HdPlugin extends Plugin implements DrawCallbacks
 		// Reset
 		gl.glBindTexture(gl.GL_TEXTURE_2D, 0);
 		gl.glBindFramebuffer(gl.GL_FRAMEBUFFER, 0);
+	}
+
+	private void initDummyShadowMap()
+	{
+		// Create texture
+		texShadowMap = glGenTexture(gl);
+		gl.glBindTexture(gl.GL_TEXTURE_2D, texShadowMap);
+		gl.glTexImage2D(gl.GL_TEXTURE_2D, 0, gl.GL_DEPTH_COMPONENT, 1, 1, 0, gl.GL_DEPTH_COMPONENT, gl.GL_FLOAT, null);
+		gl.glTexParameteri(gl.GL_TEXTURE_2D, gl.GL_TEXTURE_MIN_FILTER, gl.GL_NEAREST);
+		gl.glTexParameteri(gl.GL_TEXTURE_2D, gl.GL_TEXTURE_MAG_FILTER, gl.GL_NEAREST);
+		gl.glTexParameteri(gl.GL_TEXTURE_2D, gl.GL_TEXTURE_WRAP_S, gl.GL_CLAMP_TO_BORDER);
+		gl.glTexParameteri(gl.GL_TEXTURE_2D, gl.GL_TEXTURE_WRAP_T, gl.GL_CLAMP_TO_BORDER);
+
+		// Reset
+		gl.glBindTexture(gl.GL_TEXTURE_2D, 0);
 	}
 
 	private void shutdownShadowMapFbo()
@@ -1703,13 +1719,10 @@ public class HdPlugin extends Plugin implements DrawCallbacks
 
 			gl.glUseProgram(glProgram);
 
-			if (texShadowMap != -1)
-			{
-				// bind shadow map
-				gl.glActiveTexture(gl.GL_TEXTURE3);
-				gl.glBindTexture(GL_TEXTURE_2D, texShadowMap);
-				gl.glActiveTexture(gl.GL_TEXTURE0);
-			}
+			// bind shadow map, or dummy 1x1 texture
+			gl.glActiveTexture(gl.GL_TEXTURE3);
+			gl.glBindTexture(GL_TEXTURE_2D, texShadowMap);
+			gl.glActiveTexture(gl.GL_TEXTURE0);
 
 			if (aaEnabled)
 			{
