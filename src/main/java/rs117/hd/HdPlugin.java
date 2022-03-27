RS117/RLHD

commit 81a9886e3cc473e6ae3a2e2f57f869ae1d852ad0

fix buffer corruption after OS suspend

src/main/java/rs117/hd/HdPlugin.java
@@ -1568,6 +1568,14 @@ public class HdPlugin extends Plugin implements DrawCallbacks
 	{
 		assert jawtWindow.getAWTComponent() == client.getCanvas() : "canvas invalidated";
 
+		// reset the plugin if the last frame took >1min to draw
+		// why? because the user's computer was probably suspended and the buffers are no longer valid
+		if (System.currentTimeMillis() - lastFrameTime > 60000) {
+			log.debug("resetting the plugin after probable OS suspend");
+			shutDown();
+			startUp();
+		}
+
 		final int canvasHeight = client.getCanvasHeight();
 		final int canvasWidth = client.getCanvasWidth();
 

commit 7dfc583c0e44b037a7d81015ff15ea3a5a7669fe

Fix plugin startup loop bug
This fixes a bug introduced by 81a9886e3cc473e6ae3a2e2f57f869ae1d852ad0
where the plugin will continuously attempt to restart itself
indefinitely if the plugin had been turned off for longer than a minute.
This fixes the bug by also updating `lastFrameTime` in `startUp()`.

src/main/java/rs117/hd/HdPlugin.java
@@ -593,6 +593,8 @@ public class HdPlugin extends Plugin implements DrawCallbacks
 							gl.GL_DONT_CARE, 1, new int[]{0x20052}, 0, false);
 					}
 
+					lastFrameTime = System.currentTimeMillis();
+
 					initVao();
 					try
 					{