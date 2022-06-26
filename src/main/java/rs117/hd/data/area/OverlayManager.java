ef8f5dba29b5dc397a93ebfbf9d6b58823ef8757
Mark7625 <72366279+Mark7625@users.noreply.github.com>
2022-06-25 22:02:05 -0400

    Clean Up

--- src/main/java/rs117/hd/data/area/OverlayManager.java
+++ src/main/java/rs117/hd/data/area/OverlayManager.java
@@ -14,22 +14,16 @@ import javax.inject.Inject;
 import java.util.List;
 
 public class OverlayManager {
-    private static ListMultimap<Integer, Overlay> GROUND_MATERIAL_MAP;
+    private static ListMultimap<Integer, Overlay> GROUND_MATERIAL_MAP = ArrayListMultimap.create();
 
-    private static Overlay DEFAULT = new Overlay(-1, GroundMaterial.DIRT);
-    public static Overlay WINTER_GRASS = new Overlay(-1, GroundMaterial.SNOW_1,0,0,40,true);
+    private static final Overlay DEFAULT = new Overlay(-1, GroundMaterial.DIRT);
+    public static final Overlay WINTER_GRASS = new Overlay(-1, GroundMaterial.SNOW_1,0,0,40,true);
 
     @Inject
     HdPlugin plugin;
 
     public void loadOverlays() {
-        if(GROUND_MATERIAL_MAP == null) {
-            GROUND_MATERIAL_MAP = ArrayListMultimap.create();
-        }
         GROUND_MATERIAL_MAP.clear();
-        if(plugin.getAreaManager().currentArea != null) {
-            System.out.println(plugin.getAreaManager().currentArea.getTheme());
-        }
         plugin.getAreaManager().areas.stream().filter(it -> !it.getOverlays().isEmpty()).forEach(overlays -> {
             overlays.getOverlays().forEach(overlay -> {
                 GROUND_MATERIAL_MAP.put(overlay.getId(), overlay);
@@ -48,15 +42,13 @@ public class OverlayManager {
         }
 
         List<Overlay> overlays = GROUND_MATERIAL_MAP.get(overlayId);
-        for (Overlay overlay : overlays)
-        {
-            if (overlay.getArea().containsPoint(worldPoint))
-            {
-                return overlay;
-            }
-        }
 
-        return DEFAULT;
+        final WorldPoint finalWorldPoint = worldPoint;
+
+        return overlays.stream().filter(overlay ->
+                overlay.getArea().containsPoint(finalWorldPoint)
+        ).findFirst().orElse(DEFAULT);
+
     }
 
 }
