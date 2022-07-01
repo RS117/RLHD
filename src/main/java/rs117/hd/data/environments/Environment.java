25a840d743bbfa5195b41408e96579983d904d5a
Ezri <73786759+xxEzri@users.noreply.github.com>
2022-07-02 00:36:37 +0100

    Foundry + water stuff (#320)
    * Foundry area & environment
    
    * Environment update & waterColor
    
    * Lights
    
    * Formatting and doorway light derp
    
    * Update build.gradle
    
    * Elid environment & water fix
    
    * Update Environment.java

--- src/main/java/rs117/hd/data/environments/Environment.java
+++ src/main/java/rs117/hd/data/environments/Environment.java
@@ -899,6 +899,21 @@ public enum Environment
 	CANOE_CUTSCENE(Area.CANOE_CUTSCENE, new Properties()),
 	FISHER_KINGS_REALM(Area.FISHER_KINGS_REALM, new Properties()),
 	ENCHANTED_VALLEY(Area.ENCHANTED_VALLEY, new Properties()),
+	GIANTS_FOUNDRY(Area.GIANTS_FOUNDRY, new Properties()
+		.setFogColor(0, 0, 0)
+		.setFogDepth(12)
+		.setAmbientStrength(1.1f)
+		.setAmbientColor(255, 255, 255)
+		.setDirectionalStrength(1.0f)
+		.setDirectionalColor(255, 193, 153)
+		.setLightDirection(-113, -120f)
+		.setWaterColor(102, 234, 255)
+	),
+	ELID_CAVE(Area.ELID_CAVE, new Properties()
+			.setWaterColor(102, 234, 255)
+			.setAmbientStrength(1.75f)
+			.setDirectionalStrength(1.0f)
+	),
 
 
 
@@ -932,6 +947,7 @@ public enum Environment
 		.setDirectionalColor("#FFFFFF")
 		.setDirectionalStrength(1.0f)
 		.setLightDirection(260f, 10f)
+		.setWaterColor(102, 234, 255)
 	),
 
 	;
@@ -961,6 +977,8 @@ public enum Environment
 	private final boolean underwater;
 	private final float[] underwaterCausticsColor;
 	private final float underwaterCausticsStrength;
+	private final float[] waterColor;
+	private final boolean customWaterColor;
 
 	private static class Properties
 	{
@@ -988,6 +1006,8 @@ public enum Environment
 		private boolean underwater = false;
 		private float[] underwaterCausticsColor = null;
 		private float underwaterCausticsStrength = 0;
+		private float[] waterColor = rgb(185, 214, 255);
+		private boolean customWaterColor = false;
 
 		public Properties setFogDepth(int depth)
 		{
@@ -1022,6 +1042,13 @@ public enum Environment
 		{
 			this.ambientColor = rgb(r, g, b);
 			this.customAmbientColor = true;
+			return this;
+		}
+
+		public Properties setWaterColor(int r, int g, int b)
+		{
+			this.waterColor = rgb(r, g, b);
+			this.customWaterColor = true;
 			return this;
 		}
 
@@ -1161,6 +1188,8 @@ public enum Environment
 			properties.directionalColor : properties.underwaterCausticsColor;
 		this.underwaterCausticsStrength = properties.underwaterCausticsStrength == 0 ?
 			properties.directionalStrength : properties.underwaterCausticsStrength;
+		this.waterColor = properties.waterColor;
+		this.customWaterColor = properties.customWaterColor;
 	}
 
 	public static float[] rgb(int r, int g, int b)
