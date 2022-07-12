ee4807c78fecdc7d1b12b8930250ab360e9d5dca
hellbringer616 <m.johnsondelta@gmail.com>
2022-07-11 23:23:30 -0400

    Yanille Bank Floor
    changes the texture from gravel to a more suitable high gloss tile.

--- src/main/java/rs117/hd/data/materials/Overlay.java
+++ src/main/java/rs117/hd/data/materials/Overlay.java
@@ -143,6 +143,7 @@ public enum Overlay
 	// Yanille
 	YANILLE_BANK_TILE_1(3, Area.YANILLE_BANK, GroundMaterial.MARBLE_1_GLOSS, new Properties().setBlended(false)),
 	YANILLE_BANK_TILE_2(4, Area.YANILLE_BANK, GroundMaterial.MARBLE_2_GLOSS, new Properties().setBlended(false)),
+	YANILLE_BANK_REAR(2, Area.YANILLE_BANK, GroundMaterial.TILES_2x2_2_GLOSS, new Properties().setBlended(false).setLightness(25)),
 	YANILLE_HUNTER_SHOP_FLOOR(16, Area.YANILLE, GroundMaterial.CARPET, new Properties().setBlended(false)),
 	GUTANOTH_CAVE(29, Area.GUTANOTH_CAVE, WaterType.SWAMP_WATER_FLAT),
 
